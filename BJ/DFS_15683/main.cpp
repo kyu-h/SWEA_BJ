419d513d3ad9f7aff1edc33a188c3fe4ec754eee
kyu-h <tigerk9212@gmail.com>
2019-03-23 11:18:53 +0900

    Have to do again

--- BJ/DFS_15683/main.cpp
+++ BJ/DFS_15683/main.cpp
@@ -1,30 +1,78 @@
+//************************
+//    Have to do again
+//************************
 #include <iostream>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int n,m;
-vector<int> v;
+int row, col;
+int arr[9][9];
+int visit[9][9];
 
-void dfs(){
-    if(v.size() == m){
-        for(int i=0; i<m; i++)
-            printf("%d ", v[i]);
-        printf("\n");
+int min_ = 1000000000;
 
+int cnt_arr(){
+    int cnt = 0;
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(arr[i][j] == 0){
+                cnt++;
+            }
+        }
+    }
+
+    min_ = min(min_, cnt);
+}
+
+void dfs(int x, int y){
+    if(x == row-1 && y == col-1){
+        cnt_arr();
         return;
     }
 
-    for(int i=1; i<n+1; i++){
-        v.push_back(i);
-        dfs();
-        v.pop_back();
+
+    for(int i=0; i<x; i++){
+        for(int j=0; j<y; j++){
+            visit[i][j] = 9;
+            if(arr[i][j] == 1){
+                for()
+            }
+            if(arr[i][j] == 2){
+
+            }
+            if(arr[i][j] == 3){
+
+            }
+            if(arr[i][j] == 4){
+
+            }
+            if(arr[i][j] == 5){
+
+            }
+            dfs(i, j);
+            visit[i][j] = 0;
+        }
     }
 }
 
 int main(void){
-    cin >> n >> m;
-    dfs();
+    cin >> row >> col;
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            dfs(i, j);
+        }
+    }
+
+    cout << min_ << endl;
 
     return 0;
 }
